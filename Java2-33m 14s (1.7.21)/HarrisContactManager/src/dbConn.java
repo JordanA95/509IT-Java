50c50ede789ce38cb01535b53c0ad6064fb3e5bb
JordanA95 <47454658+JordanA95@users.noreply.github.com>
2021-07-01 20:53:21 +0100

    JavaGitHub1.7.21

---
+++ Java2-33m 14s (1.7.21)/HarrisContactManager/src/dbConn.java
@@ -0,0 +1,53 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class dbConn {
+
+	String connString ="jdbc:mysql://harris.cxw8qnxlxaco.us-east-1.rds.amazonaws.com:3306/harris";
+	String username = "admin";
+	String password = "Harrisandsons";
+	Connection con = null;
+	
+	public dbConn()
+	{
+		try {
+			con = DriverManager.getConnection(connString,username,password);
+			System.out.println("Connected");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Not Connectd");
+		}
+	}
+	public ResultSet GetAllPersonal()
+	{
+		ResultSet rs=null;
+		String sql="{call selectAllPersonal()}";
+		try {
+			java.sql.CallableStatement cst= con.prepareCall(sql);
+			rs=cst.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+	public ResultSet GetAllBusiness()
+	{
+		ResultSet rs=null;
+		String sql="{call selectAllBusiness()}";
+		try {
+			java.sql.CallableStatement cst= con.prepareCall(sql);
+			rs=cst.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	
+}
